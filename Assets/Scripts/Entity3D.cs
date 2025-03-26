28a51c3f11f1514eec9ba562f27040515959faa4
Bruno Moreira <brunomcmoreira99@gmail.com>
2025-03-26 00:15:27 +0000

    Update
    - Add Rizzo's model
    - Basic character movement logic

--- Assets/Scripts/Entity3D.cs
+++ Assets/Scripts/Entity3D.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 
 public class Entity3D : MonoBehaviour
-{
+{   
+    private GameObject _entityObject;
+    private Renderer _eObjectRenderer;
+    private float _eObjectHeight;
+
     private void Awake()
     {
+        _entityObject = transform.GetChild(0).gameObject;
+        _eObjectRenderer = _entityObject.GetComponentInChildren<Renderer>();
+        _eObjectHeight = _eObjectRenderer.bounds.min.y;
+
         transform.rotation = Quaternion.Euler(-30, 0, 0);
     }
 
@@ -19,6 +27,22 @@ public class Entity3D : MonoBehaviour
 
     private void UpdateZPosition()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * 2f);
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            (transform.position.y - _eObjectHeight) * 2
+        );
+    }
+
+    /// <summary>
+    ///   <para>Rotates the 3D object to look at the target position.</para>
+    /// </summary>
+    /// <param name="targetPos">The position to look at in world space.</param>
+    public void LookAt(Vector3 targetPos)
+    {
+        float angleRad = Mathf.Atan2(targetPos.y - transform.position.y, targetPos.x - transform.position.x);
+        Vector3 eulerRotation = _entityObject.transform.localRotation.eulerAngles;
+        eulerRotation.y = 90 - angleRad * Mathf.Rad2Deg;
+        _entityObject.transform.localRotation = Quaternion.Euler(eulerRotation);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Entity3D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+
+    private InputSystem_Actions _playerInputActions;
+    private InputSystem_Actions.PlayerActions _playerActions;
+    private Camera _cam;
+    private Rigidbody2D _rb;
+    private Entity3D _entity3D; 
+    private Vector2 _moveInput;
+    private bool _isAttacking;
+
+    private void Awake()
+    {
+        _playerInputActions = new InputSystem_Actions();
+        _playerActions = _playerInputActions.Player;
+        _cam = Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
+        _entity3D = GetComponent<Entity3D>();
+    }
+
     private void Start()
     {
         
@@ -10,5 +32,36 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * moveSpeed;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void Look()
+    {
+        Vector2 pointerPos = _cam.ScreenToWorldPoint(_playerActions.Look.ReadValue<Vector2>());
+        _entity3D.LookAt(pointerPos);
+    }
+
+    private void OnEnable() 
+    {
+        _playerActions.Enable();
+        _playerActions.Move.performed += OnMove;
+        _playerActions.Move.canceled += OnMove;
+    }
+
+    private void OnDisable() 
+    {
+        _playerActions.Move.performed -= OnMove;
+        _playerActions.Move.canceled -= OnMove;
+        _playerActions.Disable();
     }
 }
